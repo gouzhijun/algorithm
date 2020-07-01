79cd67b55c43d04866b4b75f35d370acfd8bfd15
gouzhijun <gzj2009012870@gmail.com>
2020-07-02 01:14:31 +0800

    DP-MaximumSubarray

---
+++ src/main/java/com/gouzhijun/algorithm/dp/MaximumSubarray.java
@@ -0,0 +1,45 @@
+package com.gouzhijun.algorithm.dp;
+
+/**
+ * @Author:gouzhijun
+ * @Date:2020/7/2
+ */
+public class MaximumSubarray {
+
+
+    /**
+     * Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+     * thought: max(n) = max(n-1) > 0 ? max(n-1) + nums[n] : nums[n]
+     * @param nums
+     * @return
+     */
+
+    private int max;
+
+    public int maxSubArray(int[] nums) {
+        max = nums[0];
+        maxArray(nums, nums.length - 1);
+        return max;
+    }
+
+
+    private int maxArray(int[] nums, int i) {
+        if (i == 0) {
+            return nums[0];
+        }
+        int subValue = maxArray(nums, i -1);
+        int value = subValue > 0 ? subValue + nums[i] : nums[i];
+        if (value > max) {
+            max = value;
+        }
+        return value;
+    }
+
+
+    public static void main(String[] args) {
+        MaximumSubarray maximumSubarray = new MaximumSubarray();
+        int maxValue = maximumSubarray.maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4});
+        System.out.println("max: " + maxValue);
+    }
+
+}
