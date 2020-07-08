11e38f51cb25b804ef6b35c34e2ee11aa49c93a1
gouzhijun <gouzhijun@baidu.com>
2020-07-08 20:54:10 +0800

    PartitionKEqualSubsetSum bit masking

--- src/main/java/com/gouzhijun/algorithm/dp/PartitiontoKEqualSumSubsets.java
+++ src/main/java/com/gouzhijun/algorithm/dp/PartitiontoKEqualSumSubsets.java
@@ -1,5 +1,7 @@
 package com.gouzhijun.algorithm.dp;
 
+import java.util.Arrays;
+
 /**
  * Given an array of integers nums and a positive integer k, find whether it's possible to divide this array into k non-empty subsets whose sums are all equal.
  *
@@ -51,6 +53,39 @@ public class PartitiontoKEqualSumSubsets {
             flags[i] = false;
         }
         return result;
+    }
+
+    // TODO: dynamic program & bit masking
+    // TODO: program by myself
+    // https://leetcode.com/problems/partition-to-k-equal-sum-subsets/discuss/335668/DP-with-Bit-Masking-Solution-%3A-Best-for-Interviews
+    public boolean canPartitionKSubsetsWithBitMasking(int[] nums, int k) {
+        int N = nums.length;
+        Arrays.sort(nums);
+        int sum = Arrays.stream(nums).sum();
+        int target = sum / k;
+        if (sum % k > 0 || nums[N - 1] > target) return false;
+
+        boolean[] dp = new boolean[1 << N];
+        dp[0] = true;
+        int[] total = new int[1 << N];
+
+
+
+        for (int state = 0; state < (1 << N); state++) {
+            if (!dp[state]) continue;
+            for (int i = 0; i < N; i++) {
+                int future = state | (1 << i);
+                if (state != future && !dp[future]) {
+                    if (nums[i] <= target - (total[state] % target)) {
+                        dp[future] = true;
+                        total[future] = total[state] + nums[i];
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        return dp[(1 << N) - 1];
     }
 
     public static void main(String[] args) {
